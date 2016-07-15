fa590528571978ef672658c3c240e34a82b8436e
Samuel Bowler <samuel.bowler@gmail.com>
2016-07-14 22:02:21 -0500

    added working medium dropdown

--- client/app/discovery/discovery.controller.js
+++ client/app/discovery/discovery.controller.js
@@ -12,21 +12,36 @@
       this.$scope.test = 'hello world';
 
       this.$scope.mediaList = [
-        { name: 'Music', submedia: ['Brass', 'Classical', 'Country', 'Experimental', 'Folk', 'Hip-hop', 'Jazz', 'Rock'] },
-        { name: 'Writing', submedia: ['Fiction', 'Non-Fiction', 'Poetry'] },
-        { name: 'Film', submedia: ['Documentaries', 'Feature Films', 'Short Films'] },
-        { name: 'Visual', submedia: ['Acrylics', 'Pastels', 'Watercolor', 'Charcoal', 'Pencil'] },
-        { name: 'Photography', submedia: ['Digital', 'Film'] },
-        { name: 'Dance', submedia: ['Ballet', 'Ballroom', 'Hip-hop', 'Lyrical', 'Theater', 'Plays', 'Musicals'] },
-        { name: 'Bearers', submedia: ['Mardi Gras Indians'] },
-        { name: 'Comedy', submedia: ['Dark', 'Upbeat'] },
-        { name: 'Crafts', submedia: ['Crochet', 'Cross Stitch', 'Knitting', 'Lettering Arts'] },
-        { name: 'Design' },
-        { name: 'Code', submedia: ['Front-end', 'Back-end', 'Full-stack'] },
-        { name: 'Sculpture', submedia: ['Clay', 'Metal', 'Wood'] }
+        {
+          name: 'Music',
+          submedia: ['Brass', 'Classical', 'Country', 'Experimental', 'Folk', 'Hip-hop', 'Jazz', 'Rock']
+        },
+        {name: 'Writing', submedia: ['Fiction', 'Non-Fiction', 'Poetry']},
+        {
+          name: 'Film',
+          submedia: ['Documentaries', 'Feature Films', 'Short Films']
+        },
+        {
+          name: 'Visual',
+          submedia: ['Acrylics', 'Pastels', 'Watercolor', 'Charcoal', 'Pencil']
+        },
+        {name: 'Photography', submedia: ['Digital', 'Film']},
+        {
+          name: 'Dance',
+          submedia: ['Ballet', 'Ballroom', 'Hip-hop', 'Lyrical', 'Theater', 'Plays', 'Musicals']
+        },
+        {name: 'Bearers', submedia: ['Mardi Gras Indians']},
+        {name: 'Comedy', submedia: ['Dark', 'Upbeat']},
+        {
+          name: 'Crafts',
+          submedia: ['Crochet', 'Cross Stitch', 'Knitting', 'Lettering Arts']
+        },
+        {name: 'Design'},
+        {name: 'Code', submedia: ['Front-end', 'Back-end', 'Full-stack']},
+        {name: 'Sculpture', submedia: ['Clay', 'Metal', 'Wood']}
       ];
 
-      this.$scope.loadSubMediums = function(medium) {
+      this.$scope.loadSubMediums = function (medium) {
         this.$scope.selectedMedium = medium.name;
         this.$scope.submedia = medium.submedia;
         console.log('submedia loaded');
